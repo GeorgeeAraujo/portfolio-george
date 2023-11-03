accb7c35f989aeae780bbe2bd1a402232c49640c
GeorgeeAraujo <george.programacao@gmail.com>
2023-11-03 20:47:40 -0300

    Create swiper in project`s page.

---
+++ scripts/swiperInit.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+}
+    });
