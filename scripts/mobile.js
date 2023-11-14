d884216c58e7d0f7a2d612d3a69d8746f7bdb50e
GeorgeeAraujo <george.programacao@gmail.com>
2023-11-14 10:24:08 -0300

    Update mobile version.

---
+++ scripts/mobile.js
@@ -0,0 +1,51 @@
+if(window.matchMedia("(max-width:1024px").matches){ //Verifica se é um dispositivo mobile.
+    const hambMenuIcon = document.getElementById('hamb-menu-icon');
+    const listMenu = document.getElementById('list-menu');
+    
+    hambMenuIconListener();
+
+    function toggleHambMenu(){ 
+        const classListHambMenu = Array.from(hambMenuIcon.classList);   
+        if(IsThereMenuClickedClass(classListHambMenu)){
+            hideMenuList();
+            removeMenuClickedClass(hambMenuIcon);
+        } else if (!IsThereMenuClickedClass(classListHambMenu)){
+            showMenuList();
+            addMenuClickedClass(hambMenuIcon);
+        };
+    };
+
+    function IsThereMenuClickedClass(classList){
+        return classList.includes('menu-clicked');
+    };
+
+    function showMenuList(){
+        listMenu.style.display = 'flex';
+        listMenu.style.gap = '20px';
+        listMenu.style.backgroundColor = 'var(--main-text-color)';
+        listMenu.style.width = '100%';
+        listMenu.style.padding = '0.5em';
+        listMenu.style.marginTop = '0.5em';
+        listMenu.style.position = 'absolute';
+        listMenu.style.top = '100%';
+        listMenu.style.right = '0%'
+    };
+
+    function hideMenuList(){
+        listMenu.style.display = 'none';
+    };
+
+    function addMenuClickedClass(hambMenuIcon){
+        hambMenuIcon.classList.add('menu-clicked');
+    };
+
+    function removeMenuClickedClass(hambMenuIcon){
+        hambMenuIcon.classList.remove('menu-clicked');
+    };
+
+    function hambMenuIconListener(){
+        hambMenuIcon.addEventListener('click', ()=>{
+            toggleHambMenu();
+        });
+    };
+};
